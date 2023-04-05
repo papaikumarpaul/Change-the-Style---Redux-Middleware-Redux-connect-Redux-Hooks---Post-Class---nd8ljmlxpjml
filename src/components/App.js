6b80ff949b2f08eccd5765a704f457c395c10b32
papai <Papai Pc@Lenovo.com>
2023-04-05 10:44:12 +0530

    success

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,35 @@
-import React from 'react'
-import '../styles/App.css';
-import {useSelector,useDispatch} from "react-redux";
-import {changeColor,changeStyle } from '../actions/index.js';
+import React from "react";
+import "../styles/App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { changeColor, changeStyle } from "../actions/index.js";
 
 const App = () => {
-
-const dispatch =useDispatch();
-//code here 
-
+  const dispatch = useDispatch();
+  //code here
+  // const colorState = useSelector((state) => state.value);
+  const colorState = useSelector((state) => state.value);
+  const styleState = useSelector((state) => state.value);
+  const changeButtonColor = () => {
+    // console.log("color change");
+    dispatch(changeColor(colorState === "red" ? "green" : "red"));
+  };
+  const changeButtonStyle = () => {
+    // console.log("style change");
+    dispatch(changeStyle(styleState === "Arial" ? "Georgia" : "Arial"));
+  };
   return (
     <div id="main">
-      <h1 id='text' style={{color: colorState, fontFamily: styleState}}>Newton School</h1>
-      <button id='colorBtn' onClick={()=>dispatch(changeColor(colorState))}>Change Color</button>
-      <button id='styleBtn' onClick={()=>dispatch(changeStyle(styleState))}>Change Style</button>
-
+      <h1 id="text" style={{ color: colorState, fontFamily: styleState }}>
+        Newton School
+      </h1>
+      <button id="colorBtn" onClick={changeButtonColor}>
+        Change Color
+      </button>
+      <button id="styleBtn" onClick={changeButtonStyle}>
+        Change Style
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
